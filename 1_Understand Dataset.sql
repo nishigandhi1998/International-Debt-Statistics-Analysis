91dd8e9002ef8fa18535c9cfa5c27fe1975be108
Nishi Gandhi <72174760+nishigandhi1998@users.noreply.github.com>
2023-03-20 18:05:51 +0530

    Update 1_Understand Dataset.sql

--- 1_Understand Dataset.sql
+++ 1_Understand Dataset.sql
@@ -3,6 +3,35 @@
 ## A second-level heading
 ### A third-level heading
 
+> Text that is a quote
+
+Use `git status` to list all new or modified files that haven't yet been committed.
+
+Some basic Git commands are:
+```
+git status
+git add
+git commit
+```
+The background color is `#ffffff` for light mode and `#000000` for dark mode.
+
+This site was built using [GitHub Pages](https://pages.github.com/).
+
+1. First list item
+   - First nested list item
+     - Second nested list item
+     
+1. James Madison
+2. James Monroe
+3. John Quincy Adams
+
+- George Washington
+* John Adams
++ Thomas Jefferson
+
+1.   First list item
+     - First nested list item
+
 
 **Nishi Gandhi **
 
