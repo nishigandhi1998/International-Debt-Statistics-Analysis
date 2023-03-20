c28156ac871c7b96ed7347e02f3f8489fb3bf4b7
Nishi Gandhi <72174760+nishigandhi1998@users.noreply.github.com>
2023-03-20 17:53:46 +0530

    Update 10_Other viable debt issues and conclusion.sql

--- 10_Other viable debt issues and conclusion.sql
+++ 10_Other viable debt issues and conclusion.sql
@@ -15,8 +15,7 @@ In this notebook, we took a look at debt owed by countries across the globe.
 We extracted a few summary statistics from the data and unraveled some interesting facts and figures. 
 We also validated our findings to make sure the investigations are correct.
 
-in simple term here we have find out Maximum Debt for each country 
-with respect to indicator_name */
+in simple term here we have find out For which indicator each country has maximum debt*/
 
 select * from international_debt;
 
